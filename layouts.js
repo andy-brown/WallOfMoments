22dc39a19daba33ff2253794962d4068a6e754b4
Andy Brown (Mac) <andy.brown01@bbc.co.uk>
2015-07-10 16:48:56 +0100

    moved layout definitions to layouts.js

---
+++ layouts.js
@@ -0,0 +1,10 @@
+// layout definitions
+var layouts = {
+	1:
+	[
+		{ left: 0, top: 0, width: 0.5, height: 0.5 },
+		{ left: 0.5, top: 0, width: 0.5, height: 0.5 },
+		{ left: 0, top: 0.5, width: 0.5, height: 0.5 },
+		{ left: 0.5, top: 0.5, width: 0.5, height: 0.5 }
+	]
+};
