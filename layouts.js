96ccecf0d73afe88ffd4088b94bd2de3fed6737a
Andy Brown (Mac) <andy.brown01@bbc.co.uk>
2015-07-10 22:06:04 +0100

    added 3x3 to layouts

--- layouts.js
+++ layouts.js
@@ -1,10 +1,28 @@
+/*
+	define possible layouts to use for walls
+	sizes are given as proportions of overall available space
+	(so keep this sensible!)
+*/
+
 // layout definitions
 var layouts = {
-	1:
+	1: // simple 2x2
 	[
 		{ left: 0, top: 0, width: 0.5, height: 0.5 },
 		{ left: 0.5, top: 0, width: 0.5, height: 0.5 },
 		{ left: 0, top: 0.5, width: 0.5, height: 0.5 },
 		{ left: 0.5, top: 0.5, width: 0.5, height: 0.5 }
+	],
+	2: // simple 3x3
+	[
+		{ left: 0, top: 0, width: 0.33, height: 0.33 },
+		{ left: 0.33, top: 0, width: 0.33, height: 0.33 },
+		{ left: 0.66, top: 0, width: 0.33, height: 0.33 },
+		{ left: 0, top: 0.33, width: 0.33, height: 0.33 },
+		{ left: 0.33, top: 0.33, width: 0.33, height: 0.33 },
+		{ left: 0.66, top: 0.33, width: 0.33, height: 0.33 },
+		{ left: 0, top: 0.66, width: 0.33, height: 0.33 },
+		{ left: 0.33, top: 0.66, width: 0.33, height: 0.33 },
+		{ left: 0.66, top: 0.66, width: 0.33, height: 0.33 },
 	]
 };
