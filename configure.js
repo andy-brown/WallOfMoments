156befae134568d211052ddd9de69c6291381263
Andy Brown (Mac) <andy.brown01@bbc.co.uk>
2015-08-19 12:26:27 +0100

    implemented config saving

--- configure.js
+++ configure.js
@@ -373,6 +373,25 @@ document.addEventListener('keydown', function(ev){
 // save the config
 function save(){
 	// save newConfig as json
+	var url = "saveConf.php";
+	var filename = prompt("Please enter a name for this wall");
+	if(filename === null){
+		return;
+	}
+	filename = filename + ".json";
+	var params = "fname=" + filename + "&conf=" + JSON.stringify(newConfig);
+	var http = new XMLHttpRequest();
+	http.open("POST", url, true);
+
+	//Send the proper header information along with the request
+	http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+	http.onreadystatechange = function() {//Call a function when the state changes.
+		if(http.readyState == 4 && http.status == 200) {
+			alert(http.responseText);
+		}
+	}
+	http.send(params);
 	console.log(JSON.stringify(newConfig));
 }
 
