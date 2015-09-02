94c81c200f0db1d0da9a25c242c5234e83de4d03
Andy Brown (Mac) <andy.brown01@bbc.co.uk>
2015-09-02 09:28:54 +0100

    improved borders by making layout more precise

--- layouts.js
+++ layouts.js
@@ -27,11 +27,11 @@ var layouts = {
 	],
 	3: // 6 box with top left twice size
 	[
-		{ left: 0, top: 0, width: 0.67, height: 0.67 },
-		{ left: 0.67, top: 0, width: 0.33, height: 0.34 },
-		{ left: 0.67, top: 0.34, width: 0.33, height: 0.33 },
-		{ left: 0, top: 0.67, width: 0.34, height: 0.33 },
-		{ left: 0.34, top: 0.67, width: 0.33, height: 0.33 },
-		{ left: 0.67, top: 0.67, width: 0.33, height: 0.33 }
+		{ left: 0, top: 0, width: 0.6667, height: 0.6667 },
+		{ left: 0.6667, top: 0, width: 0.3333, height: 0.3333 },
+		{ left: 0.6667, top: 0.3333, width: 0.3333, height: 0.3333 },
+		{ left: 0, top: 0.6667, width: 0.3333, height: 0.3333 },
+		{ left: 0.3333, top: 0.6667, width: 0.3333, height: 0.3333 },
+		{ left: 0.6667, top: 0.6667, width: 0.3333, height: 0.3333 }
 	]
 };
